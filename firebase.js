662c04dceddbe27251f67ff0b67a3fd5b815500c
mastanbi551 <mastanbi551sk@gmail.com>
2023-07-19 12:05:07 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8SiJN0sKVFklhWhaIF6mDii06CLWOtMM",
+  authDomain: "qrcodeapp-975af.firebaseapp.com",
+  projectId: "qrcodeapp-975af",
+  storageBucket: "qrcodeapp-975af.appspot.com",
+  messagingSenderId: "344054194576",
+  appId: "1:344054194576:web:731d3bbb15eebc7d309315"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
